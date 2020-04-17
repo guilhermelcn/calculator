ce67f4a608de56dedf4e6cb6e7e8d352bf4c2d9b
Guilherme Lucio <guilcn62@gmail.com>
2020-04-17 13:56:39 -0300

    fix:(removeConsole) removing console.log

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -71,7 +71,6 @@ export default class Calculator extends Component {
       const values = [...this.state.values]
       values[i] = newValue
       this.setState({ values })
-      console.log(values)
     }
   }
 
